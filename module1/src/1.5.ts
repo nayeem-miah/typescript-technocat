3bc8e4dee930e024cb02d98ccde2a7946c330949
nayeem-miah <nayeem5113a@gmail.com>
2025-06-19 20:15:38 +0600

    object types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,29 @@
+//  reference types --> object
+
+const user: {
+    company: "programming Hero"; //type ---> literal type
+    firstName: string;
+    middleName?: string;// ? optional type
+    lastName: string;
+    isMired: boolean;
+} = {
+    company : "programming Hero",
+    firstName: "Jhankar",
+    lastName: "Mahabub",
+    isMired: false
+};
+
+//  recap
+const person: {
+    name: string;
+    age?: number;
+    company: "Bangladesh It"
+} = {
+    name: "Abdul Hamid",
+    age: 23,
+    company: "Bangladesh It"
+};
+
+
+person.age = 43;
+console.log(person);
