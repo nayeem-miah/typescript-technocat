a02a255ba631baec4bf09fd8d02d75434d085b63
nayeem-miah <nayeem5113a@gmail.com>
2025-06-23 10:54:17 +0600

    create class and constrctor

---
+++ module3/3.1.ts
@@ -0,0 +1,31 @@
+{   
+    // oop
+
+    // create class
+    class Animal {
+        // name: string;
+        // species: string;
+        // sound: string;
+
+        // constructor
+        constructor(public name: string,public species: string,public sound: string) {
+            this.name = name;
+            this.species = species;
+            this.sound = sound
+        };
+
+        // crate method
+        soundMake() {
+            console.log(`this ${this.name} sound is ${this.sound}`); 
+        }
+    };
+
+
+    // Access property and method
+    const cat = new Animal("USA winds", "cat", "mew mew ........");
+    const dog = new Animal("bd dog", "dog", "gaw gaw gaw............");
+    cat.name;
+    cat.soundMake();
+    dog.soundMake();
+    // 
+};
