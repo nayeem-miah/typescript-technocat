373bfe150a172623316a1105c90f134bfbc48ac7
nayeem-miah <nayeem5113a@gmail.com>
2025-06-26 12:43:48 +0600

    static

---
+++ module3/3.7.ts
@@ -0,0 +1,31 @@
+{
+    //  static and dynamically
+    class Counter {
+        static count: number = 0;
+
+        static increment() {
+            return (Counter.count = Counter.count + 1)
+        };
+        // increment() {
+        //     return (Counter.count = Counter.count + 1)
+        // };
+
+
+        // decrement() {
+        //     return (Counter.count = Counter.count - 1);
+        // };
+
+        static decrement() {
+            return (Counter.count = Counter.count - 1);
+        };
+    };
+    // const instance1 = new Counter();
+    console.log(Counter.increment());  // 1
+    console.log(Counter.increment()); // 2 
+    // console.log(instance1.increment()); // 3
+
+    // const instance2 = new Counter();
+    console.log(Counter.increment());  // 1  --> using static then Counter.count 4
+    // console.log(instance2.increment()); // 2 --> using static then Counter.count 5
+
+}
