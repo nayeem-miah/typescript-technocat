677b9c58989d4fdeb9b41e1a0282888e9b69eff2
nayeem-miah <nayeem5113a@gmail.com>
2025-06-27 20:26:08 +0600

    problem 2 done

--- problemSolving/problemSolving.ts
+++ problemSolving/problemSolving.ts
@@ -16,7 +16,28 @@ function formateString(input: string, toUpper?: boolean): string {
 const inputValue = formateString("Hello", false);
 const inputValue1 = formateString("hello");
 const inputValue2 = formateString("hello", true);
-console.log(inputValue2);
+// console.log(inputValue2);
 
 
 //  problem 1 done-------------------------
+
+
+// problem 2 -----------------------
+// Description: Create a function that filters an array of objects by the rating property, returning only items with a rating of 4 or more.
+
+type Items = {
+    title: string;
+    rating: number
+}
+function filterByRating(items: Items[]): Items[] {
+    return items.filter((item) => item.rating >= 4);
+};
+const books = [
+    { title: "Book A", rating: 4.5 },
+    { title: "Book B", rating: 3.2 },
+    { title: "Book C", rating: 5.0 }
+];
+
+const filterRating = filterByRating(books);
+// console.log(filterRating);
+// problem 2 done-------------------------
