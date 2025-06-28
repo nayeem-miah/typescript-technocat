b445e33f070da387a6b8cf2702eb5b875de16f9c
nayeem-miah <nayeem5113a@gmail.com>
2025-06-28 16:15:59 +0600

    problem 7 done

--- problemSolving/problemSolving.ts
+++ problemSolving/problemSolving.ts
@@ -221,3 +221,28 @@ function examMonth(month: Month): string {
 
 // problem 7 done -------------------
 
+
+
+
+
+
+// problem 8
+// Description: Create an async function that:
+// Returns the square of a number after 1 second
+// Rejects if the number is negative
+
+async function squareAsync(n: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+        if (n < 0) {
+            return reject(new Error("negative number is not allow"));
+        }
+
+        setTimeout(() => {
+            resolve(n * n);
+        }, 1000);
+    });
+}
+
+
+squareAsync(4).then(console.log);
+squareAsync(-3).catch(console.error); 
